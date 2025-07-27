91af8451a7cda341db49ab824632cb0e0b6bb3bc
mfa <www.faiz83988@gmail.com>
2025-07-28 00:52:30 +0700

    Add missing import os

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
